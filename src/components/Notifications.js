5f098ecf323b1ac4d9e033457f56bb4751aa639a
Logan Moss <lrdjmoss@gmail.com>
2020-05-04 21:31:39 -0700

    Added working updates to indexhtml and also notifications recipe components

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import "./style.css";
+import moment from 'moment';
 
-function Notifications(){
-    return <h2>Notifications Component</h2>
+function Notifications(props){
+    return(
+        <div className="container">
+            <div className="row">
+            {props.ingredients ? props.ingredients.map(item => {
+                if(item.date_expire){
+                    item.date_expire = moment().format('DD-MM-YYYY')
+                    console.log(moment().format(item.date_expire) - moment().calendar())
+                }
+                return(
+                    <div></div>
+                )
+                }):""}
+            </div>
+        </div>
+    )
 }
 
 export default Notifications
