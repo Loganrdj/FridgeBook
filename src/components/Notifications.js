af342f2f82c1ddfc209da82541c3bee2786a2a29
Fung <koufongso@gmail.com>
2020-05-09 11:02:58 -0700

    added expiration date alert

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,12 +1,12 @@
 import React, { useState, Component } from 'react';
 import "./style.css";
-import Alert from 'react-bootstrap/Alert';
+import IngredientAlert from './IngredientAlert';
 
 class Notifications extends Component {
-    
-    constructor(props){
+
+    constructor(props) {
         super(props)
-        this.state = {props, show: true, ingredients: ""}
+        this.state = { props, show: true, ingredients: "" }
     }
 
     handleFalse = (id) => {
@@ -19,37 +19,17 @@ class Notifications extends Component {
     // const [show, setShow] = useState(true);
     // const handleFalse = () => setShow(false);
     // const handleTrue = () => setShow(true);
-    render(){
-        return(
+    render() {
+        return (
             <div className="container">
                 <div className="row">
-                {this.props.ingredients ? this.props.ingredients.map(item => {
-                    if(item.date_expire){
-                        // Store Today as DATE Today Time 00:00:00 Store Expiration date (input) as date 00:00:00
-                        let tempDate = item.date_expire.substring(0,item.date_expire.length-1)
-                        let remain = Math.floor(((new Date(tempDate) - new Date())/86400000)+ 1);
-                        if(remain === 1 || remain === 2 ){
-                            return(
-                                <Alert variant={'info'} show={this.state.show} key={item.id} onClick={this.handleFalse} dismissible>
-                                    {item.name} will expire in {remain} amount of day(s)!
-                                </Alert>
-                            )
-                        } else if(remain === 0){
-                            return(
-                                <Alert variant={'warning'} show={this.state.show} key={item.id} onClick={this.handleFalse} dismissible>
-                                    {item.name} will expire today!
-                                </Alert>
-                            )
-                        } else if(remain < 0){
-                            return(
-                                <Alert variant={'danger'} show={this.state.show} key={item.id} onClick={this.handleFalse} dismissible>
-                                    {item.name} is expired!
-                                </Alert>
-                            )
-                        }
-                    }
-                    return null;
-                    }):""}
+                    {this.props.ingredients ? this.props.ingredients.map(item => {
+                        if (item.date_expire) {
+                            // Store Today as DATE Today Time 00:00:00 Store Expiration date (input) as date 00:00:00
+                            let tempDate = item.date_expire.substring(0, item.date_expire.length - 1)
+                            let remain = Math.floor(((new Date(tempDate) - new Date()) / 86400000) + 1);
+                            return (<IngredientAlert key={item.id} item={item} remain={remain}></IngredientAlert>);
+                        }}) : ""}
                 </div>
             </div>
         )
