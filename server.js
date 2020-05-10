312215cb0e3c1c1a69e897a3786a4f6b17f006b1
Logan Moss <lrdjmoss@gmail.com>
2020-05-10 15:40:40 -0700

    PackageJSON bug fixes

--- server.js
+++ server.js
@@ -19,12 +19,15 @@ app.use(express.json());
 var session = require("express-session"),
   bodyParser = require("body-parser");
 
+  
 app.use(express.static("public"));
 app.use(session({ secret: myKeys.cookieSession.sessioinKey })); //cookie: { maxAge: 0.5 * 60 * 60 * 1000 } }
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(passport.session());
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+}
 app.use(router);                                   //"backend routing/api call"
 
 
